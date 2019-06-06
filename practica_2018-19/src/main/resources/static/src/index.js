4088c34ff8e34d5b765786d581aad43e99e59238
TALLEST SHORT KING 5'9" angel <43203190+aaangell@users.noreply.github.com>
2019-06-06 22:53:17 +0200

    index js

--- practica_2018-19/src/main/resources/static/src/index.js
+++ practica_2018-19/src/main/resources/static/src/index.js
@@ -24,7 +24,7 @@ window.onload = function() {
 
 	// WEBSOCKET CONFIGURATOR
 	game.global.socket = new WebSocket("ws://192.168.0.18:8090/spacewar")
-	
+
 	game.global.socket.onopen = () => {
 		if (game.global.DEBUG_MODE) {
 			console.log('[DEBUG] WebSocket connection opened.')
@@ -36,10 +36,10 @@ window.onload = function() {
 			console.log('[DEBUG] WebSocket connection closed.')
 		}
 	}
-	
+
 	game.global.socket.onmessage = (message) => {
 		var msg = JSON.parse(message.data)
-		
+
 		switch (msg.event) {
 		case 'JOIN':
 			if (game.global.DEBUG_MODE) {
@@ -54,81 +54,81 @@ window.onload = function() {
 			break
 		case 'JOIN ROOM':
 			if (msg.respuesta=="jugador ha entrado"){
-                console.log(msg.roomName);
-            }
-            else{
-                console.log("error")
-            }
+				console.log(msg.roomName);
+			}
+			else{
+				console.log("error")
+			}
 			break
 		case 'NEW ROOM' :
-            if (game.global.DEBUG_MODE) {
-                console.log('[DEBUG] NEW ROOM message recieved')
-                console.dir(msg)
-            }
-            if (msg.respuesta=="Sala creada"){
-                game.global.myPlayer.room = msg.room
-                console.log("Creaste la sala: " + game.global.myPlayer.room)
-            }
-            else{
-                console.log("error")
-            }
-            break
+			if (game.global.DEBUG_MODE) {
+				console.log('[DEBUG] NEW ROOM message recieved')
+				console.dir(msg)
+			}
+			if (msg.respuesta=="Sala creada"){
+				game.global.myPlayer.room = msg.room
+				console.log("Creaste la sala: " + game.global.myPlayer.room)
+			}
+			else{
+				console.log("error")
+			}
+			break
 		case 'ROOMS':
-            if(msg.numSalas=="0"){
-                console.log("aun no hay salas")
-            }
-            else{
-                if(msg.numSalas != game.global.numRooms){
-                	 game.global.numRooms = msg.numSalas;
-                	 game.global.refreshRooms = true
-                }
-               
-                for (var sala of msg.salas) 
-                {
-                	var igual = false
-            		var i = 0
+			if(msg.numSalas=="0"){
+				console.log("aun no hay salas")
+			}
+			else{
+				if(msg.numSalas != game.global.numRooms){
+					game.global.numRooms = msg.numSalas;
+					game.global.refreshRooms = true
+				}
 
-                	if(sala.tipo == "2"){
-                		while((!igual)&&(i<game.global.onevsoneRoom.length))
-                		{
-                			igual = (game.global.onevsoneRoom[i].nombre == sala.nombre)
-                			i++
-                		}
+				for (var sala of msg.salas) 
+				{
+					var igual = false
+					var i = 0
 
-                		if(!igual)
-                		{
-                			game.global.onevsoneRoom[i] = sala
-                		}
-                	}
-                	else if(sala.tipo == "20")
-                	{
-                		while((!igual)&&(i<game.global.battleRoom.length))
-                		{
-                			igual = (game.global.battleRoom[i].nombre == sala.nombre)
-                			i++
-                		}
+					if(sala.tipo == "2"){
+						while((!igual)&&(i<game.global.onevsoneRoom.length))
+						{
+							igual = (game.global.onevsoneRoom[i].nombre == sala.nombre)
+							i++
+						}
 
-                		if(!igual)
-                		{
-                			game.global.battleRoom[i] = sala
-                		}
-            		}
-                }
-                
-                for(var sala of msg.salas)
-                {
-                	console.log("/IndexDebug/ Nombre sala jugador: " + game.global.myPlayer.room)
-                	console.log("/IndexDebug/ Nombre sala comprobada: " + sala.nombre)
-                	if(sala.nombre == game.global.myPlayer.room)
-                	{
-                		if(sala.jugadores == "2")
-                		{
-                			game.global.beginGame = true
-                		}
-                	}
-                }
-            }
-            break
+						if(!igual)
+						{
+							game.global.onevsoneRoom[i] = sala
+						}
+					}
+					else if(sala.tipo == "20")
+					{
+						while((!igual)&&(i<game.global.battleRoom.length))
+						{
+							igual = (game.global.battleRoom[i].nombre == sala.nombre)
+							i++
+						}
+
+						if(!igual)
+						{
+							game.global.battleRoom[i] = sala
+						}
+					}
+				}
+
+				for(var sala of msg.salas)
+				{
+					console.log("/IndexDebug/ Nombre sala jugador: " + game.global.myPlayer.room)
+					console.log("/IndexDebug/ Nombre sala comprobada: " + sala.nombre)
+					if(sala.nombre == game.global.myPlayer.room)
+					{
+						if(sala.jugadores == "2")
+						{
+							game.global.beginGame = true
+						}
+					}
+				}
+			}
+			break
 		case 'PLAYER MSG' :
 			var textarea = document.getElementById('chat');
 			var today = new Date();
@@ -143,7 +143,7 @@ window.onload = function() {
 				game.global.pointRanking[i] = player.record;
 				i++;
 			}
-			
+
 			game.global.refreshRank = true
 			break
 		case 'GAME STATE UPDATE' :
@@ -153,45 +153,52 @@ window.onload = function() {
 			}
 			if (typeof game.global.myPlayer.image !== 'undefined') {
 				for (var player of msg.players) {
-					if (game.global.myPlayer.id == player.id) {
-						game.global.myPlayer.image.x = player.posX
-						game.global.myPlayer.image.y = player.posY
-						game.global.myPlayer.image.angle = player.facingAngle
-					} else {
-						if (typeof game.global.otherPlayers[player.id] == 'undefined') {
-							game.global.otherPlayers[player.id] = {
-									image : game.add.sprite(player.posX, player.posY, 'spacewar', player.shipType)
-							}
-							game.global.otherPlayers[player.id].image.anchor.setTo(0.5, 0.5)
+					console.log("myRom"+game.global.myPlayer.room)
+					console.log("expected rom"+player.room)
+					
+					if((game.global.myPlayer.room==player.room)&&(player.room!="")){
+						if (game.global.myPlayer.id == player.id) {
+							game.global.myPlayer.image.x = player.posX
+							game.global.myPlayer.image.y = player.posY
+							game.global.myPlayer.image.angle = player.facingAngle
+							game.global.myPlayer.fuel=player.fuel
 						} else {
-							game.global.otherPlayers[player.id].image.x = player.posX
-							game.global.otherPlayers[player.id].image.y = player.posY
-							game.global.otherPlayers[player.id].image.angle = player.facingAngle
+							if (typeof game.global.otherPlayers[player.id] == 'undefined') {
+								game.global.otherPlayers[player.id] = {
+										image : game.add.sprite(player.posX, player.posY, 'spacewar', player.shipType)
+								}
+								game.global.otherPlayers[player.id].image.anchor.setTo(0.5, 0.5)
+							} else {
+								game.global.otherPlayers[player.id].image.x = player.posX
+								game.global.otherPlayers[player.id].image.y = player.posY
+								game.global.otherPlayers[player.id].image.angle = player.facingAngle
+							}
 						}
 					}
 				}
-				
+
 				for (var projectile of msg.projectiles) {
-					if (projectile.isAlive) {
-						game.global.projectiles[projectile.id].image.x = projectile.posX
-						game.global.projectiles[projectile.id].image.y = projectile.posY
-						if (game.global.projectiles[projectile.id].image.visible === false) {
-							game.global.projectiles[projectile.id].image.angle = projectile.facingAngle
-							game.global.projectiles[projectile.id].image.visible = true
-						}
-					} else {
-						if (projectile.isHit) {
-							
-							console.log("Colision")
-							
-							// we load explosion
-							let explosion = game.add.sprite(projectile.posX, projectile.posY, 'explosion')
-							explosion.animations.add('explosion')
-							explosion.anchor.setTo(0.5, 0.5)
-							explosion.scale.setTo(2, 2)
-							explosion.animations.play('explosion', 15, false, true)
-							
-							/*
+					if((game.global.myPlayer.room==projectile.ownerRoom)&&(player.room!="")){
+						if (projectile.isAlive) {
+							game.global.projectiles[projectile.id].image.x = projectile.posX
+							game.global.projectiles[projectile.id].image.y = projectile.posY
+							if (game.global.projectiles[projectile.id].image.visible === false) {
+								game.global.projectiles[projectile.id].image.angle = projectile.facingAngle
+								game.global.projectiles[projectile.id].image.visible = true
+							}
+						} else {
+							if (projectile.isHit) {
+
+								console.log("Colision")
+
+								// we load explosion
+								let explosion = game.add.sprite(projectile.posX, projectile.posY, 'explosion')
+								explosion.animations.add('explosion')
+								explosion.anchor.setTo(0.5, 0.5)
+								explosion.scale.setTo(2, 2)
+								explosion.animations.play('explosion', 15, false, true)
+
+								/*
 							if(game.global.otherPlayers[player.id] !== undefined)
 							{
 								console.log("lol")
@@ -200,9 +207,24 @@ window.onload = function() {
 							else{
 								game.global.myPlayer.image.destroy()
 							}*/
-							
+
+							}
+							game.global.projectiles[projectile.id].image.visible = false
 						}
-						game.global.projectiles[projectile.id].image.visible = false
+					}
+				}
+				for (var hit of msg.hits){
+					if(game.global.myPlayer.id == hit.id){
+						game.global.myPlayer.vida=hit.vida;
+					}
+					else if(game.global.otherPlayers[hit.id]!==undefined){
+						game.global.otherPlayer.vida=hit.vida;
+					}
+					if(game.global.myPlayer.id==hit.hitBy){
+						game.global.myPlayer.puntuacion=hit.point;
+					}
+					else if(game.global.otherPlayers[hit.hitBy]!==undefined){
+						game.global.otherPlayers[hit.hitBy].puntuacion=hit.point;
 					}
 				}
 			}
